d086a49831102b36c65815ca2da5976d28fb425a
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-09-17 16:08:27 +0200

    wip

--- src/transforming_resolver/mod.rs
+++ src/transforming_resolver/mod.rs
@@ -1,14 +1,18 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use crate::Resolver;
 use arrow::{
     array::{ArrayRef, LargeStringArray},
-    datatypes::DataType,
     record_batch::RecordBatch,
 };
 use async_trait::async_trait;
+use sqlparser::{
+    ast::{Expr, Ident, ObjectName, SelectItem, SetExpr, Statement, TableAlias, TableFactor},
+    dialect::PostgreSqlDialect,
+    parser::Parser,
+};
 
-pub trait Transformation {
+pub trait Transformation: Send + Sync {
     fn apply(&self, data: ArrayRef) -> ArrayRef;
 }
 
@@ -27,19 +31,77 @@ impl Transformation for ReplaceString {
 
 pub struct TransformingResolver {
     resolver: Box<dyn Resolver>,
+    transformations: HashMap<String, Vec<Box<dyn Transformation>>>,
 }
 
 impl TransformingResolver {
     pub fn new(resolver: Box<dyn Resolver>) -> TransformingResolver {
-        TransformingResolver { resolver }
+        TransformingResolver {
+            resolver,
+            transformations: HashMap::new(),
+        }
     }
 
     pub fn add_transformation(
-        self,
+        mut self,
         target_field: &str,
         transformation: Box<dyn Transformation>,
     ) -> TransformingResolver {
+        let field_transformations = self
+            .transformations
+            .entry(target_field.to_string())
+            .or_insert(vec![]);
+
+        field_transformations.push(transformation);
+
         self
+    }
+}
+
+fn transform_record_batch(
+    data: RecordBatch,
+    column_transformations: HashMap<usize, &Vec<Box<dyn Transformation>>>,
+) -> RecordBatch {
+    if column_transformations.keys().len() == 0 {
+        return data;
+    }
+
+    let arrays: Vec<ArrayRef> = (0..data.num_columns())
+        .map(|idx| {
+            let column_data = data.column(idx).clone();
+            match column_transformations.get(&idx) {
+                Some(transformations) => transformations
+                    .iter()
+                    .fold(column_data, |data, transformation| {
+                        transformation.apply(data)
+                    }),
+                None => column_data,
+            }
+        })
+        .collect();
+
+    RecordBatch::try_new(data.schema(), arrays).unwrap()
+}
+
+impl TransformingResolver {
+    fn get_column_transformations(
+        &self,
+        statement: &Statement,
+        data: &RecordBatch,
+    ) -> HashMap<usize, &Vec<Box<dyn Transformation>>> {
+        let normalized_field_names = get_schema_fields(statement).unwrap();
+
+        data.schema()
+            .fields()
+            .iter()
+            .zip(normalized_field_names.iter())
+            .enumerate()
+            .filter_map(|(index, (_, normalized_field_name))| {
+                self.transformations
+                    .get(normalized_field_name)
+                    .map(|transformations| (index, transformations))
+            })
+            .collect()
     }
 }
 
@@ -54,13 +116,18 @@ impl Resolver for TransformingResolver {
         client_id: uuid::Uuid,
         query: String,
     ) -> anyhow::Result<arrow::record_batch::RecordBatch> {
+        let dialect = PostgreSqlDialect {};
+        let query_ast = Parser::parse_sql(&dialect, &query).unwrap();
+
+        if query_ast.len() != 1 {
+            todo!("Mismatched number of statements");
+        }
+
         let result = self.resolver.query(client_id, query).await;
         result.map(|data| {
-            for field in data.schema().fields() {
-                println!("{:?}", field)
-            }
-
-            data
+            let column_transformations =
+                self.get_column_transformations(query_ast.first().unwrap(), &data);
+            transform_record_batch(data, column_transformations)
         })
     }
 
@@ -126,5 +193,143 @@ impl Resolver for TransformingResolver {
 
     async fn terminate(&mut self, client_id: uuid::Uuid) -> anyhow::Result<()> {
         self.resolver.terminate(client_id).await
+    }
+}
+
+fn get_schema_fields(ast: &Statement) -> anyhow::Result<Vec<String>> {
+    match ast {
+        Statement::Query(query) => match &query.body {
+            SetExpr::Select(select) => {
+                let mut fields: Vec<String> = select
+                    .projection
+                    .iter()
+                    .map(|item| match item {
+                        SelectItem::UnnamedExpr(Expr::Identifier(Ident {
+                            value,
+                            quote_style: _,
+                        })) => value.clone(),
+                        SelectItem::UnnamedExpr(Expr::CompoundIdentifier(identifiers)) => {
+                            identifiers
+                                .iter()
+                                .map(|item| item.value.to_string())
+                                .collect::<Vec<String>>()
+                                .join(".")
+                        }
+                        SelectItem::ExprWithAlias {
+                            expr:
+                                Expr::Identifier(Ident {
+                                    value,
+                                    quote_style: _,
+                                }),
+                            alias: _,
+                        } => value.clone(),
+                        _ => unimplemented!(),
+                    })
+                    .collect();
+
+                if select.from.len() == 1 {
+                    match select.from.first().unwrap().clone().relation {
+                        TableFactor::Table {
+                            name: ObjectName(name_identifiers),
+                            alias,
+                            args: _,
+                            with_hints: _,
+                        } => {
+                            if name_identifiers.len() != 1 {
+                                unimplemented!()
+                            }
+
+                            let original_name = name_identifiers.clone().pop().unwrap().value;
+
+                            match alias {
+                                Some(TableAlias {
+                                    name: aliased_name,
+                                    columns: _,
+                                }) => {
+                                    fields = fields
+                                        .iter()
+                                        .map(|field| {
+                                            field.replace(
+                                                format!("{}.", aliased_name.value).as_str(),
+                                                format!("{}.", original_name).as_str(),
+                                            )
+                                        })
+                                        .collect()
+                                }
+                                None => {
+                                    fields = fields
+                                        .iter()
+                                        .map(|field| format!("{}.{}", original_name, field))
+                                        .collect()
+                                }
+                            }
+                        }
+                        _ => unimplemented!(),
+                    }
+                } else {
+                    unimplemented!()
+                }
+
+                Ok(fields)
+            }
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_schema_fields() {
+        let dialect = PostgreSqlDialect {};
+        let query_ast = Parser::parse_sql(&dialect, "SELECT id FROM users")
+            .unwrap()
+            .pop()
+            .unwrap();
+
+        let unnested_fields = get_schema_fields(&query_ast).unwrap();
+        assert_eq!(unnested_fields, vec!["users.id"])
+    }
+
+    #[test]
+    fn test_schema_fields_renamed() {
+        let dialect = PostgreSqlDialect {};
+        let query_ast = Parser::parse_sql(&dialect, "SELECT id AS 'user_id' FROM users")
+            .unwrap()
+            .pop()
+            .unwrap();
+
+        let unnested_fields = get_schema_fields(&query_ast).unwrap();
+        assert_eq!(unnested_fields, vec!["users.id"])
+    }
+
+    #[test]
+    fn test_schema_fields_table_alias() {
+        let dialect = PostgreSqlDialect {};
+        let query_ast = Parser::parse_sql(&dialect, "SELECT u.id FROM users u")
+            .unwrap()
+            .pop()
+            .unwrap();
+
+        let unnested_fields = get_schema_fields(&query_ast).unwrap();
+        assert_eq!(unnested_fields, vec!["users.id"])
+    }
+
+    #[test]
+    fn test_schema_fields_join() {
+        let dialect = PostgreSqlDialect {};
+        let query_ast = Parser::parse_sql(
+            &dialect,
+            "SELECT u.id, p.id, p.title FROM users u JOIN posts p",
+        )
+        .unwrap()
+        .pop()
+        .unwrap();
+
+        let unnested_fields = get_schema_fields(&query_ast).unwrap();
+        assert_eq!(unnested_fields, vec!["users.id", "posts.id", "posts.title"])
     }
 }
