9caa4e8e3eed156bddc3587b5561bfbcfbd93533
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-10-20 12:17:51 +0200

    feat: Handling postgres name arrays

--- crates/proboscis-core/src/utils/arrow.rs
+++ crates/proboscis-core/src/utils/arrow.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
-use arrow::array::as_primitive_array;
+use arrow::array::{as_primitive_array, make_array, ArrayData, ListArray};
+use arrow::array::{Array, GenericListArray, UInt8Array};
 use arrow::array::{ArrayRef, GenericStringArray, Int16Array, Int32Array, Int64Array, Int8Array};
-use arrow::datatypes::{DataType, Field, Schema};
+use arrow::buffer::Buffer;
+use arrow::datatypes::{DataType, Field, Schema, ToByteSlice, UInt8Type};
 use arrow::record_batch::RecordBatch;
 use omnom::prelude::*;
 use postgres_protocol::message::{DataRow, RowDescription};
@@ -92,6 +94,24 @@ fn column_data_to_array(data: &[Vec<u8>], data_type: &DataType) -> ArrayRef {
                 data_array.iter().map(|s| s.as_ref()).collect::<Vec<&str>>(),
             ))
         }
+        DataType::List(field) => {
+            let data_array: Vec<Option<Vec<Option<u8>>>> = data
+                .iter()
+                .map(|d| Some(d.iter().map(|d| Some(*d)).collect()))
+                .collect();
+
+            let array = ListArray::from_iter_primitive::<UInt8Type, _, _>(data_array);
+
+            let list_data_type = DataType::List(field.clone());
+
+            let list_data = ArrayData::builder(list_data_type)
+                .len(array.len())
+                .add_buffer(Buffer::from(array.value_offsets().to_byte_slice()))
+                .add_child_data(array.values().data().clone())
+                .build();
+
+            make_array(list_data)
+        }
         _ => unimplemented!(),
     }
 }
@@ -122,41 +142,63 @@ fn protocol_rows_to_arrow_columns(
     Ok(result)
 }
 
-fn arrow_type_for_postgres_type(postgres_type: &postgres::types::Type) -> DataType {
-    match postgres_type {
-        &postgres::types::Type::BOOL => DataType::Boolean,
-        &postgres::types::Type::INT2 => DataType::Int8,
-        &postgres::types::Type::INT4 => DataType::Int16,
-        &postgres::types::Type::INT8 => DataType::Int32,
-        &postgres::types::Type::TEXT => DataType::LargeUtf8,
-        &postgres::types::Type::VARCHAR => DataType::Utf8,
-        _ => unimplemented!(),
-    }
-}
-
-fn postgres_type_for_arrow_type(arrow_type: &DataType) -> postgres::types::Type {
+fn postgres_type_for_arrow_type(
+    arrow_type: &DataType,
+    original_type: Option<postgres::types::Type>,
+) -> postgres::types::Type {
     match arrow_type {
         DataType::Boolean => postgres::types::Type::BOOL,
         DataType::Int8 => postgres::types::Type::INT2,
         DataType::Int16 => postgres::types::Type::INT4,
         DataType::Int32 => postgres::types::Type::INT8,
         DataType::LargeUtf8 => postgres::types::Type::TEXT,
-        DataType::Utf8 => postgres::types::Type::VARCHAR,
-        _ => unimplemented!(),
+        DataType::Utf8 => match original_type {
+            Some(postgres::types::Type::NAME) => postgres::types::Type::NAME,
+            _ => postgres::types::Type::VARCHAR,
+        },
+        DataType::List(field) => match field.name().as_str() {
+            "unnamed_name_array" => postgres::types::Type::NAME_ARRAY,
+            _ => todo!("{}", arrow_type),
+        },
+        _ => todo!("{}", arrow_type),
     }
 }
 
 fn message_field_to_arrow_field(value: &postgres_protocol::message::Field) -> Field {
     let postgres_type = postgres::types::Type::from_oid(value.type_oid as u32).unwrap();
-    let arrow_type = arrow_type_for_postgres_type(&postgres_type);
+
+    let arrow_type = match &postgres_type {
+        &postgres::types::Type::BOOL => DataType::Boolean,
+        &postgres::types::Type::INT2 => DataType::Int8,
+        &postgres::types::Type::INT4 => DataType::Int16,
+        &postgres::types::Type::INT8 => DataType::Int32,
+        &postgres::types::Type::TEXT => DataType::LargeUtf8,
+        &postgres::types::Type::VARCHAR => DataType::Utf8,
+        &postgres::types::Type::NAME => DataType::Utf8,
+        &postgres::types::Type::NAME_ARRAY => DataType::List(Box::new(Field::new(
+            "unnamed_name_array",
+            DataType::UInt8,
+            true,
+        ))),
+        _ => todo!("{}", postgres_type),
+    };
 
     let mut metadata = BTreeMap::new();
     metadata.insert("table_oid".to_string(), format!("{}", value.table_oid));
+    metadata.insert(
+        "column_number".to_string(),
+        format!("{}", value.column_number),
+    );
     metadata.insert("format".to_string(), format!("{}", value.format));
     metadata.insert("type_length".to_string(), format!("{}", value.type_length));
     metadata.insert(
         "type_modifier".to_string(),
         format!("{}", value.type_modifier),
+    );
+
+    metadata.insert(
+        "original_type_oid".to_string(),
+        postgres_type.oid().to_string(),
     );
 
     let mut field = Field::new(&value.name, arrow_type, false);
@@ -165,15 +207,29 @@ fn message_field_to_arrow_field(value: &postgres_protocol::message::Field) -> Fi
     field
 }
 
-fn arrow_field_to_message_field(
-    value: &Field,
-    column_number: i16,
-) -> postgres_protocol::message::Field {
-    let postgres_type = postgres_type_for_arrow_type(value.data_type());
+fn arrow_field_to_message_field(value: &Field) -> postgres_protocol::message::Field {
+    let original_type = postgres::types::Type::from_oid(
+        value
+            .metadata()
+            .clone()
+            .unwrap()
+            .get("original_type_oid")
+            .unwrap()
+            .parse()
+            .unwrap(),
+    );
+
+    let postgres_type = postgres_type_for_arrow_type(value.data_type(), original_type);
 
     let metadata = value.metadata().clone().unwrap();
 
     let table_oid = metadata.get("table_oid").unwrap().clone().parse().unwrap();
+    let column_number = metadata
+        .get("column_number")
+        .unwrap()
+        .clone()
+        .parse()
+        .unwrap();
     let format = metadata.get("format").unwrap().clone().parse().unwrap();
 
     // TODO: These can probably be inferred based on some criteria
@@ -284,6 +340,22 @@ pub fn serialize_record_batch_to_data_rows(batch: &RecordBatch) -> Vec<DataRow> 
                         let value = values.value(row_index);
                         cell.extend_from_slice(value.as_bytes())
                     }
+                    DataType::List(_) => {
+                        let values = &column
+                            .as_any()
+                            .downcast_ref::<GenericListArray<i32>>()
+                            .unwrap();
+
+                        let row_value = values.value(row_index);
+
+                        let value = row_value
+                            .as_any()
+                            .downcast_ref::<UInt8Array>()
+                            .unwrap()
+                            .values();
+
+                        cell.extend_from_slice(value)
+                    }
                     _ => unimplemented!(),
                 }
                 row_data.push(cell)
@@ -301,7 +373,83 @@ pub fn serialize_record_batch_schema_to_row_description(schema: &Schema) -> RowD
         .fields()
         .iter()
         .enumerate()
-        .map(|(idx, arrow_field)| arrow_field_to_message_field(arrow_field, idx as i16))
+        .map(|(idx, arrow_field)| arrow_field_to_message_field(arrow_field))
         .collect();
     RowDescription { fields }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+    use std::iter;
+
+    #[test]
+    fn test_name_array() {
+        let row_data: Vec<Vec<Vec<u8>>> =
+            iter::repeat(vec![rand::thread_rng().gen::<[u8; 32]>().to_vec()])
+                .take(4)
+                .collect();
+
+        let list_data_type = DataType::List(Box::new(Field::new(
+            "unnamed_name_array",
+            DataType::UInt8,
+            true,
+        )));
+
+        let schema = Schema::new(vec![Field::new("some_list", list_data_type, true)]);
+        let columns = protocol_rows_to_arrow_columns(&schema, row_data.clone()).unwrap();
+        let batch = RecordBatch::try_new(Arc::new(schema), columns).unwrap();
+
+        let deserialized = serialize_record_batch_to_data_rows(&batch);
+
+        assert_eq!(4, deserialized.len());
+
+        let byte_rows: Vec<Vec<Vec<u8>>> = deserialized
+            .iter()
+            .map(|data_row| data_row.field_data.clone())
+            .collect();
+
+        assert_eq!(row_data, byte_rows);
+    }
+
+    #[test]
+    fn test_symmetric_serialization_deserialization() {
+        let fields = vec![
+            postgres_protocol::message::Field {
+                name: "a".to_string(),
+                table_oid: 0,
+                column_number: 0,
+                type_oid: 19,
+                type_length: 64,
+                type_modifier: -1,
+                format: 0,
+            },
+            postgres_protocol::message::Field {
+                name: "b".to_string(),
+                table_oid: 0,
+                column_number: 0,
+                type_oid: 1003,
+                type_length: -1,
+                type_modifier: -1,
+                format: 0,
+            },
+        ];
+
+        let data = vec![DataRow {
+            field_data: vec![
+                vec![112, 111, 115, 116, 103, 114, 101, 115],
+                vec![123, 112, 117, 98, 108, 105, 99, 125],
+            ],
+        }];
+
+        let batch = simple_query_response_to_record_batch(&fields, &data).unwrap();
+
+        let deserialized_row_description =
+            serialize_record_batch_schema_to_row_description(&batch.schema());
+        let deserialized_data = serialize_record_batch_to_data_rows(&batch);
+
+        assert_eq!(fields, deserialized_row_description.fields);
+        assert_eq!(data, deserialized_data);
+    }
 }
