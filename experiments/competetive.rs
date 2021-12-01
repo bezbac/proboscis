bf12a385864c653443b68211b2651af83ba7c545
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-12-01 16:10:14 +0100

    wip

--- experiments/competetive.rs
+++ experiments/competetive.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use postgres::NoTls;
 use proboscis_resolver_postgres::TargetConfig;
 use std::{
@@ -212,11 +213,30 @@ fn simple_query(connection_url: &str) {
 }
 
 fn benchmark_function(function: &dyn Fn() -> ()) {
-    let before = Instant::now();
-    for _ in 0..1000 {
-        function()
+    let iterations = 1000;
+    let before_all = Instant::now();
+
+    let mut milis = vec![];
+
+    for _ in 0..iterations {
+        let before_each = Instant::now();
+        function();
+        milis.push(before_each.elapsed().as_millis());
     }
-    println!("Elapsed time: {:.2?}", before.elapsed());
+
+    println!(
+        "Total time ({} iterations): {:.2?}",
+        iterations,
+        before_all.elapsed()
+    );
+
+    println!(
+        "Avg time: {:.2?}ms",
+        milis.iter().sum1::<u128>().unwrap() / milis.len() as u128
+    );
+
+    println!("Min time: {:.2?}ms", milis.iter().min().unwrap());
+    println!("Max time: {:.2?}ms", milis.iter().max().unwrap());
 }
 
 fn main() {
