a501894f60a033236a43fb1e22add76650081614
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-12-02 12:29:30 +0100

    wip

--- experiments/utils/benchmark.rs
+++ experiments/utils/benchmark.rs
@@ -1,8 +1,33 @@
-use std::time::Instant;
+use std::{
+    process::Command,
+    sync::mpsc::{channel, TryRecvError},
+    thread,
+    time::{Duration, Instant},
+};
 
 use itertools::Itertools;
 
 pub fn benchmark_function(function: &dyn Fn() -> ()) {
+    let (tx, rx) = channel();
+    let receiver = thread::spawn(move || loop {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("docker stats --no-stream")
+            .output()
+            .expect("failed to execute process");
+
+        dbg!(output);
+
+        thread::sleep(Duration::from_millis(100));
+        match rx.try_recv() {
+            Ok(_) | Err(TryRecvError::Disconnected) => {
+                println!("Terminating.");
+                break;
+            }
+            Err(TryRecvError::Empty) => {}
+        }
+    });
+
     let iterations = 1000;
     let before_all = Instant::now();
 
@@ -27,4 +52,7 @@ pub fn benchmark_function(function: &dyn Fn() -> ()) {
 
     println!("Min time: {:.2?}ms", milis.iter().min().unwrap());
     println!("Max time: {:.2?}ms", milis.iter().max().unwrap());
+
+    let _ = tx.send(());
+    receiver.join().expect("The receiver thread has panicked");
 }
