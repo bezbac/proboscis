2beb9f7bb8799e39df574af84516e23a5570c904
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-12-08 17:07:25 +0100

    wip

--- experiments/competetive_latency.rs
+++ experiments/competetive_latency.rs
@@ -80,75 +80,55 @@ fn main() {
     print_benchmark_stats(&pgbouncer_durations);
     drop(_pgbouncer_node);
 
+    let result_labels = vec!["no proxy", "pgcloak", "pgpool", "pgbouncer"];
+    let result_durations = vec![baseline_durations, pgcloak_durations, pgpool_durations, pgbouncer_durations];
+
     #[cfg(feature = "analysis")]
     {
         use inline_python::python;
         use itertools::Itertools;
         use std::time::Instant;
 
-        fn calculate_total_time(durations: &[(Instant, Instant)]) -> u128 {
-            let first_time = durations.first().unwrap().1;
-            let last_time = durations.last().unwrap().0;
-            return last_time.duration_since(first_time).as_millis();
-        }
+        let total_times: Vec<u128> = result_durations
+            .iter()
+            .map(|durations| {
+                let first_time = durations.first().unwrap().1;
+                let last_time = durations.last().unwrap().0;
+                last_time.duration_since(first_time).as_millis()
+            })
+            .collect();
 
-        let mut total_times = vec![];
-        total_times.push(calculate_total_time(&baseline_durations));
-        total_times.push(calculate_total_time(&pgcloak_durations));
-        total_times.push(calculate_total_time(&pgpool_durations));
-        total_times.push(calculate_total_time(&pgbouncer_durations));
-
-        // fn calculate_stats(durations: &[(Instant, Instant)]) -> (u128, u128, u128) {
-        //     let durations_in_milis: Vec<u128> = durations
-        //         .iter()
-        //         .map(|(end, start)| end.duration_since(*start).as_millis())
-        //         .collect();
-
-        //     let mean = durations_in_milis.iter().sum1::<u128>().unwrap()
-        //         / durations_in_milis.len() as u128;
-        //     let min = *durations_in_milis.iter().min().unwrap();
-        //     let max = *durations_in_milis.iter().max().unwrap();
-
-        //     (mean, min, max)
-        // }
-
-        // let mut stats = vec![];
-        // stats.push(calculate_stats(&baseline_durations));
-        // stats.push(calculate_stats(&pgcloak_durations));
-        // stats.push(calculate_stats(&pgpool_durations));
-        // stats.push(calculate_stats(&pgbouncer_durations));
-
-        fn durations_in_milis(durations: &[(Instant, Instant)]) -> Vec<u128> {
-            durations
-                .iter()
-                .map(|(end, start)| end.duration_since(*start).as_millis())
-                .collect()
-        }
-
-        let baseline_durations_in_ms = durations_in_milis(&baseline_durations);
-        let pgcloak_durations_in_ms = durations_in_milis(&pgcloak_durations);
-        let pgpool_durations_in_ms = durations_in_milis(&pgpool_durations);
-        let pgbouncer_durations_in_ms = durations_in_milis(&pgbouncer_durations);
+        let durations_in_milis: Vec<Vec<u128>> = result_durations
+            .iter()
+            .map(|durations| {
+                durations
+                    .iter()
+                    .map(|(end, start)| end.duration_since(*start).as_millis())
+                    .collect()
+            })
+            .collect();
 
         python! {
             import matplotlib.pyplot as plt
             import numpy as np
             import seaborn as sns
-
             from matplotlib.ticker import FormatStrFormatter
 
             # Total Duration
             fig, ax = plt.subplots()
             plt.title("Total benchmark duration (%s iterations)" % 'iterations)
-            labels = ["no-proxy", "pgcloak", "pgpool", "pgbouncer"]
 
-            ax.bar(labels, 'total_times)
+            ax.bar('result_labels, 'total_times)
             ax.bar_label(ax.containers[0])
             ax.get_yaxis().set_major_formatter(FormatStrFormatter("%d ms"))
 
             # Violin Plot
             fig, ax = plt.subplots()
-            ax = sns.violinplot(data=['baseline_durations_in_ms, 'pgcloak_durations_in_ms, 'pgpool_durations_in_ms, 'pgbouncer_durations_in_ms])
+            plt.title("Individual run duration (%s iterations)" % 'iterations)
+
+            ax = sns.violinplot(data='durations_in_milis)
+            ax.set_xticklabels('result_labels)
+            ax.get_yaxis().set_major_formatter(FormatStrFormatter("%d ms"))
 
             plt.show()
         }
