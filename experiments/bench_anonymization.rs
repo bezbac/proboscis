89de5460ee72dd34a4cea86b1159bd0762872e3a
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-12-08 16:36:32 +0100

    wip

--- experiments/bench_anonymization.rs
+++ experiments/bench_anonymization.rs
@@ -10,11 +10,33 @@ use postgres::{Client, NoTls};
 use std::fs;
 use testcontainers::clients::{self};
 
+const QUERY: &str = "SELECT age, sex, race, workclass, education FROM adults";
+
 fn seed_database(database_connection_url: &str) {
     let mut client = Client::connect(database_connection_url, NoTls).unwrap();
     let adults_data = "./experiments/resources/adults.sql";
     let contents = fs::read_to_string(adults_data).expect("Something went wrong reading the file");
     client.batch_execute(&contents).unwrap();
+}
+
+fn benchmark(docker: &clients::Cli, database_connection_url: &str, config: &PgcloakConfig) {
+    let (pgcloak_connection_url, _pgcloak_node, _pgcloak_tempdir) =
+        start_pgcloak(docker, database_connection_url, config);
+
+    let result = query_data_into_dataframe(&pgcloak_connection_url, QUERY);
+    println!("{}", result.head(Some(12)));
+
+    let (docker_stats, baseline_durations) =
+        utils::docker::stats::while_collecting_docker_stats(_pgcloak_node.id(), &|| {
+            utils::benchmark::benchmark_function(20, &|| {
+                let mut client = Client::connect(&pgcloak_connection_url, NoTls).unwrap();
+                client.query(QUERY, &[]).unwrap();
+            })
+        });
+    print_benchmark_stats(&baseline_durations);
+
+    drop(_pgcloak_node);
+    drop(_pgcloak_tempdir);
 }
 
 fn main() {
@@ -24,14 +46,11 @@ fn main() {
 
     seed_database(&database_connection_url);
 
-    let query = "SELECT age, sex, race, workclass, education FROM adults";
-
-    let result = query_data_into_dataframe(&database_connection_url, query);
+    let result = query_data_into_dataframe(&database_connection_url, QUERY);
     println!("{}", result.head(Some(12)));
 
-    // PGCLOAK: Example
     println!("pgcloak baseline");
-    let (pgcloak_connection_url, _pgcloak_node, _pgcloak_tempdir) = start_pgcloak(
+    benchmark(
         &docker,
         &database_connection_url,
         &PgcloakConfig {
@@ -41,24 +60,8 @@ fn main() {
         },
     );
 
-    let result = query_data_into_dataframe(&pgcloak_connection_url, query);
-    println!("{}", result.head(Some(12)));
-
-    let (docker_stats, baseline_durations) =
-        utils::docker::stats::while_collecting_docker_stats(_pgcloak_node.id(), &|| {
-            utils::benchmark::benchmark_function(20, &|| {
-                let mut client = Client::connect(&pgcloak_connection_url, NoTls).unwrap();
-                client.query(query, &[]).unwrap();
-            })
-        });
-    print_benchmark_stats(&baseline_durations);
-
-    drop(_pgcloak_node);
-    drop(_pgcloak_tempdir);
-
-    // PGCLOAK: Example with anonymization
     println!("pgcloak k-anonymization | k=3 | QI columns: age, sex, race, workclass, education");
-    let (pgcloak_connection_url, _pgcloak_node, _pgcloak_tempdir) = start_pgcloak(
+    benchmark(
         &docker,
         &database_connection_url,
         &PgcloakConfig {
@@ -84,18 +87,57 @@ fn main() {
         },
     );
 
-    let result = query_data_into_dataframe(&pgcloak_connection_url, query);
-    println!("{}", result.head(Some(12)));
+    println!("pgcloak k-anonymization | k=10 | QI columns: age, sex, race, workclass, education");
+    benchmark(
+        &docker,
+        &database_connection_url,
+        &PgcloakConfig {
+            columns: vec![
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.age"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.sex"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.race"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.workclass"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.education"),
+                },
+            ],
+            max_pool_size: 10,
+            k: 10,
+        },
+    );
 
-    let (docker_stats, baseline_durations) =
-        utils::docker::stats::while_collecting_docker_stats(_pgcloak_node.id(), &|| {
-            utils::benchmark::benchmark_function(20, &|| {
-                let mut client = Client::connect(&pgcloak_connection_url, NoTls).unwrap();
-                client.query(query, &[]).unwrap();
-            })
-        });
-    print_benchmark_stats(&baseline_durations);
-
-    drop(_pgcloak_node);
-    drop(_pgcloak_tempdir);
+    println!("pgcloak k-anonymization | k=30 | QI columns: age, sex, race, workclass, education");
+    benchmark(
+        &docker,
+        &database_connection_url,
+        &PgcloakConfig {
+            columns: vec![
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.age"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.sex"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.race"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.workclass"),
+                },
+                ColumnConfiguration::PseudoIdentifier {
+                    name: String::from("adults.education"),
+                },
+            ],
+            max_pool_size: 10,
+            k: 10,
+        },
+    );
 }
