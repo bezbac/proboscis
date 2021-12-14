156892db1c2e9d16db5f551bafbd735a42c68ce5
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2021-12-14 14:10:41 +0100

    wip

--- experiments/bench_anonymization.rs
+++ experiments/bench_anonymization.rs
@@ -1,20 +1,14 @@
 mod utils;
 
-use crate::utils::benchmark::print_benchmark_stats;
 use crate::utils::data::query_data_into_dataframe;
 use crate::utils::docker::pgcloak::start_pgcloak;
 use crate::utils::docker::pgcloak::ColumnConfiguration;
 use crate::utils::docker::pgcloak::PgcloakConfig;
 use crate::utils::docker::postgres::start_dockerized_postgres;
-use arrow::datatypes::Float64Type;
-use ndarray_stats::EntropyExt;
 use polars::prelude::DataFrame;
-use polars::prelude::DataType;
-use polars::prelude::Utf8Type;
 use postgres::{Client, NoTls};
-use std::collections::HashMap;
 use std::fs;
-use std::slice::SliceIndex;
+use std::time::Duration;
 use std::time::Instant;
 use testcontainers::clients::{self};
 
@@ -27,61 +21,11 @@ fn seed_database(database_connection_url: &str) {
     client.batch_execute(&contents).unwrap();
 }
 
-fn count_equivalence_classes(df: &DataFrame, columns: &[&str]) -> usize {
-    let df = df.select(columns.to_vec()).unwrap();
-    let df = df.drop_duplicates(true, None).unwrap();
-    df.height()
-}
-
-fn get_equivalence_class_sizes(df: &DataFrame) -> Vec<i32> {
-    let mut merged = vec![];
-    for series in df.get_columns() {
-        for (idx, v) in series
-            .cast::<Utf8Type>()
-            .unwrap()
-            .utf8()
-            .unwrap()
-            .into_iter()
-            .enumerate()
-        {
-            if idx >= merged.len() {
-                merged.push(String::from(""))
-            }
-
-            merged[idx] = format!("{};{:?}", merged[idx], v)
-        }
-    }
-
-    let mut map = HashMap::new();
-    for value in merged {
-        let counter = map.entry(value).or_insert(0);
-        *counter += 1;
-    }
-
-    return map.values().cloned().collect();
-}
-
-fn calculate_discernibility_metric(df: &DataFrame, columns: &[&str]) -> usize {
-    let df = df.select(columns.to_vec()).unwrap();
-    let eq_class_sizes = get_equivalence_class_sizes(&df);
-
-    assert_eq!(
-        eq_class_sizes.len(),
-        count_equivalence_classes(&df, &df.get_column_names())
-    );
-
-    eq_class_sizes
-        .iter()
-        .fold(0, |agg, value| agg + (value * value) as usize)
-}
-
 fn benchmark(
     docker: &clients::Cli,
     database_connection_url: &str,
     config: &PgcloakConfig,
-    result_qi_columns: &[String],
-    baseline: &DataFrame,
-) {
+) -> (DataFrame, Duration) {
     let (pgcloak_connection_url, _pgcloak_node, _pgcloak_tempdir) =
         start_pgcloak(docker, database_connection_url, config);
 
@@ -105,35 +49,7 @@ fn benchmark(
     drop(_pgcloak_node);
     drop(_pgcloak_tempdir);
 
-    let query_duration = after.duration_since(before).as_millis();
-    println!("Query duration {}ms", query_duration);
-
-    if result_qi_columns.len() > 0 {
-        let qi_columns_strs = result_qi_columns
-            .iter()
-            .map(|c| c.as_str())
-            .collect::<Vec<&str>>();
-        let equivalence_class_count = count_equivalence_classes(&result, &qi_columns_strs);
-        println!("Equivalence class count {:?}", equivalence_class_count);
-
-        let average_eq_size = result.height() / equivalence_class_count;
-        println!("Average eq size {:?}", average_eq_size);
-
-        let normalized_average_eq_size = average_eq_size / config.k;
-        println!(
-            "Normalized average eq size {:?}",
-            normalized_average_eq_size
-        );
-
-        let dm = calculate_discernibility_metric(&result, &qi_columns_strs);
-        println!("Discernibility_metric {:?}", dm);
-    }
-
-    // TODO: KL-Divergence
-    // let baseline_ndarray = baseline.to_ndarray::<Float64Type>().unwrap();
-    // let ndarray = result.to_ndarray::<Float64Type>().unwrap();
-    // let kl_divergence = ndarray.kl_divergence(&baseline_ndarray).unwrap();
-    // println!("KL-Divergence {}", kl_divergence);
+    (result, after.duration_since(before))
 }
 
 fn main() {
@@ -156,8 +72,6 @@ fn main() {
             columns: vec![],
             max_pool_size: 10,
         },
-        &[],
-        &baseline_result,
     );
 
     let column_configs = vec![
@@ -177,6 +91,12 @@ fn main() {
             name: String::from("adults.education"),
         },
     ];
+
+    let mut result_labels = vec![];
+    let mut result_data = vec![];
+    let mut result_ks = vec![];
+    let mut result_columns = vec![];
+    let mut result_durations = vec![];
 
     for i in 0..3 {
         let end = column_configs.len() - (2 - i);
@@ -199,7 +119,7 @@ fn main() {
                 k,
                 column_names.join(", ")
             );
-            benchmark(
+            let (data, duration) = benchmark(
                 &docker,
                 &database_connection_url,
                 &PgcloakConfig {
@@ -207,9 +127,139 @@ fn main() {
                     max_pool_size: 10,
                     k,
                 },
-                &column_names,
-                &baseline_result,
             );
+
+            result_labels.push(format!(
+                "k-anonymization | k={} | QI columns: {}",
+                k,
+                column_names.join(", ")
+            ));
+            result_data.push(data);
+            result_columns.push(column_names);
+            result_ks.push(k);
+            result_durations.push(duration);
+        }
+    }
+
+    #[cfg(feature = "analysis")]
+    {
+        use inline_python::python;
+        use itertools::Itertools;
+        use polars::prelude::Utf8Type;
+        use std::collections::HashMap;
+
+        fn count_equivalence_classes(df: &DataFrame, columns: &[&str]) -> usize {
+            let df = df.select(columns.to_vec()).unwrap();
+            let df = df.drop_duplicates(true, None).unwrap();
+            df.height()
+        }
+
+        fn get_equivalence_class_sizes(df: &DataFrame) -> Vec<i32> {
+            let mut merged = vec![];
+            for series in df.get_columns() {
+                for (idx, v) in series
+                    .cast::<Utf8Type>()
+                    .unwrap()
+                    .utf8()
+                    .unwrap()
+                    .into_iter()
+                    .enumerate()
+                {
+                    if idx >= merged.len() {
+                        merged.push(String::from(""))
+                    }
+
+                    merged[idx] = format!("{};{:?}", merged[idx], v)
+                }
+            }
+
+            let mut map = HashMap::new();
+            for value in merged {
+                let counter = map.entry(value).or_insert(0);
+                *counter += 1;
+            }
+
+            return map.values().cloned().collect();
+        }
+
+        fn calculate_discernibility_metric(df: &DataFrame, columns: &[&str]) -> usize {
+            let df = df.select(columns.to_vec()).unwrap();
+            let eq_class_sizes = get_equivalence_class_sizes(&df);
+
+            assert_eq!(
+                eq_class_sizes.len(),
+                count_equivalence_classes(&df, &df.get_column_names())
+            );
+
+            eq_class_sizes
+                .iter()
+                .fold(0, |agg, value| agg + (value * value) as usize)
+        }
+
+        let durations_in_milis: Vec<u128> = result_durations
+            .iter()
+            .map(|duration| duration.as_millis())
+            .collect();
+
+        let mut eq_class_counts = vec![];
+        let mut average_eq_class_sizes = vec![];
+        let mut normalized_average_eq_class_sizes = vec![];
+        let mut discernibility_metrics = vec![];
+
+        for index in 0..result_data.len() {
+            let k = result_ks.get(index).unwrap();
+            let data = result_data.get(index).unwrap();
+            let columns = result_columns.get(index).unwrap();
+
+            if columns.len() > 0 {
+                let qi_columns_strs = columns.iter().map(|c| c.as_str()).collect::<Vec<&str>>();
+
+                let equivalence_class_count = count_equivalence_classes(&data, &qi_columns_strs);
+                eq_class_counts.push(Some(equivalence_class_count));
+
+                let average_eq_size = data.height() / equivalence_class_count;
+                average_eq_class_sizes.push(Some(average_eq_size));
+
+                let normalized_average_eq_size = average_eq_size / k;
+                normalized_average_eq_class_sizes.push(Some(normalized_average_eq_size));
+
+                let dm = calculate_discernibility_metric(&data, &qi_columns_strs);
+                discernibility_metrics.push(Some(dm));
+
+                // TODO: KL-Divergence
+                // let baseline_ndarray = baseline.to_ndarray::<Float64Type>().unwrap();
+                // let ndarray = result.to_ndarray::<Float64Type>().unwrap();
+                // let kl_divergence = ndarray.kl_divergence(&baseline_ndarray).unwrap();
+                // println!("KL-Divergence {}", kl_divergence);
+            } else {
+                eq_class_counts.push(None);
+                average_eq_class_sizes.push(None);
+                normalized_average_eq_class_sizes.push(None);
+                discernibility_metrics.push(None);
+            }
+        }
+
+        python! {
+            import matplotlib.pyplot as plt
+            import numpy as np
+            import seaborn as sns
+            from matplotlib.ticker import FormatStrFormatter
+            from matplotlib.ticker import FuncFormatter
+
+            def format_ms(x, pos):
+                if x > 1800:
+                    return "{0:.2f} s".format(x / 1000)
+                return "{0} ms".format(x)
+
+            fig, ax = plt.subplots()
+            ax.set_title("Query durations")
+            ax.bar('result_labels, 'durations_in_milis)
+            ax.bar_label(ax.containers[0])
+            ax.get_yaxis().set_major_formatter(FuncFormatter(format_ms))
+            ax.set_axisbelow(True)
+            ax.get_yaxis().grid(True, color="#EEEEEE")
+
+            plt.show()
         }
     }
 }
